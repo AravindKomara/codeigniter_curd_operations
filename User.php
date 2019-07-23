991aad72a1193893057953974190eb437d6bdfcb
AravindKomara <53178882+AravindKomara@users.noreply.github.com>
2019-07-23 18:16:59 +0530

    aravind

--- User.php
+++ User.php
@@ -30,32 +30,52 @@ class User extends CI_Controller {
         return $resultcurl;//returning the data
     }
     
+    private function commonImageUpload($imagename,$upload_path){
+        if(!empty($_FILES[$imagename]['name'])){
+              $config['upload_path'] = "./uploads/users/images";
+              $config['allowed_types'] = 'gif|jpg|png|jpeg';//image extensions
+              $config['max_size'] = '2000';//image size
+              $config['max_width'] = '1500';
+              $config['max_height'] = '1500';
+              $config['overwrite'] = false;
+              $this->load->library('upload', $config);//loading upload library of codeigniter
+              $this->upload->initialize($config);  
+              if($this->upload->do_upload($imagename)){ // picture is name of your input filed in view  
+                  $imageData = $this->upload->data();//getting image details
+                  return  $upload_path.$imageData['raw_name'].$imageData['file_ext'];
+
+        }else{
+            $error = array('error' => $this->upload->display_errors());
+            return $error;
+        }
+        }
+    }
+    
+    private  function phpAlert($msg) {
+    echo '<script type="text/javascript">alert("' . $msg . '")</script>';
+}
+    
     public function userRegistration(){
         $data = array("uname"=>$this->input->post('uname'),
             "upassword"=>md5($this->input->post('upassword')),
             "uemail"=>$this->input->post('uemail'),
             "umobile"=>$this->input->post('umobile'));
-        if(!empty($_FILES['user_image']['name'])){
-              $config['upload_path'] = "./uploads/users/images";
-              $config['allowed_types'] = 'gif|jpg|png|jpeg';//image extensions
-              $config['max_size'] = '5120';//image size
-              $config['max_width'] = '1024';
-              $config['max_height'] = '768';
-              $config['overwrite'] = false;
-              $this->load->library('upload', $config);//loading upload library of codeigniter
-              $this->upload->initialize($config);  
-              if($this->upload->do_upload('user_image')){ // picture is name of your input filed in view  
-                  $imageData = $this->upload->data();//getting image details
-                 $data['upicture'] = '' . base_url() . 'uploads/users/images/' . $imageData['raw_name'] . $imageData['file_ext'];
-
+        $upload_path = 'uploads/users/images/';
+        $imageres = $this->commonImageUpload("user_image",$upload_path);
+        if(is_array($imageres)){
+              print_r(strip_tags($imageres['error']));exit;
         }else{
-            echo $this->upload->display_errors();
-            $data['upicture'] =  "";
+            $data['upicture'] = $imageres;
         }
-        }
-        //print_r(json_encode($data));exit;
-        $res = $this->operation->userRegistration($data);
+       
+        $res = $this->operation->commonInsert("user_details",$data);
         echo $res;
+        //Storing insertion status message.
+           /* if($res){
+                $this->session->set_flashdata('success_msg', 'User data have been added successfully.');
+            }else{
+                $this->session->set_flashdata('error_msg', 'Some problems occured, please try again.');
+            }*/
         
     }
     
@@ -64,33 +84,29 @@ class User extends CI_Controller {
     }
     
     public function userLogin(){
-        $emailcolumn = "uemail";
-        $passwordcolumn = "upassword";
-        $data = array( "uemail"=>$this->input->post('uemail'),
+        $where = array( "uemail"=>$this->input->post('uemail'),
             "upassword"=>md5($this->input->post('upassword')));
         // print_r($data);exit;
-       $res['userdetails'] = $this->operation->userLogin($emailcolumn,$passwordcolumn,$data);
+       $res['userdetails'] = $this->operation->commonGet($where,"user_details","single");
        $this->load->view('update',$res);
        // echo $res;
     }
     
     public function userUpdateDetails(){
-        $idcloumnname = "uid";
-        $uid = $this->input->post('user_id');
-        $data = array("uname"=>$this->input->post('uname'),
+        $where = array( "uid"=>$this->input->post('user_id'));
+        $set = array("uname"=>$this->input->post('uname'),
             "upassword"=>$this->input->post('upassword'),
             "uemail"=>$this->input->post('uemail'),
             "umobile"=>$this->input->post('umobile'));
        // print_r($data);exit;
      
-       $res = $this->operation->userUpdateDetails($idcloumnname,$uid,$data);
+       $res = $this->operation->commonUpdate($where,$set,'user_details');
        echo  $res;
     }
     
     public function userDelete(){
-        $idcloumnname = "uid";
-        $uid = $this->input->post('user_id');
-        $res = $this->operation->userDelete($idcloumnname,$uid);
+        $where = array("uid"=>$this->input->post('user_id'));
+        $res = $this->operation->commonDelete($where,'user_details');
         echo $res;
     }
 }
